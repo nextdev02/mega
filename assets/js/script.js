de088e9679b3aa9c7fa66e3a5fe85e89b379542b
nextdev2 <nextdev0.02@gmail.com>
2023-11-27 22:00:27 +0600

    header-update

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,21 @@ let clinets = new Swiper(".clients__brands", {
 });
 
 
+/**
+ * add Event on elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
 // Sticky Header
 
 const header = document.querySelector("header")
@@ -57,14 +72,31 @@ window.addEventListener("scroll", function () {
 });
 
 
-// Header For Mobile
-const toggleBtn = document.querySelector('.toggle_btn');
-const dropdownMenu = document.querySelector('.dropdown_menu');
+/**
+ * navbar toggle
+ */
 
-toggleBtn.addEventListener('click', function () {
-  console.log('Button clicked'); // Add this line
-  dropdownMenu.classList.toggle('open');
-});
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
 
 
 
